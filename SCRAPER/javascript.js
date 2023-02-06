44bf363a0e6d8539e6e14feccfee7561b1a92698
LeonardoMarini <100464046+LeonardoMarini@users.noreply.github.com>
2023-02-06 21:43:23 +0100

    Update javascript.js

--- SCRAPER/javascript.js
+++ SCRAPER/javascript.js
@@ -1,58 +1,27 @@
-<?php
-global $response;
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-
-
-getPath();
-getData();
-getNews();
-
-echo json_encode($response);
-function crawl_page($icone)
-{
-    $breaker = false;
-    foreach ($icone as $element) {
-        if ($breaker) {
-            break;
-        }
-        // Extracting value of src attribute of
-        // the current image object
-        $src = $element->getAttribute('src');
-        // Given Output as image with extracted attribute,
-        // you can print value of those attributes also
-        //echo $src;
-        return $src;
-        //$breaker = true;
-
+function fetchScraper(){
+    fetch('http://localhost/SCRAPER/scraper.php')
+  .then((response) => response.json())
+  .then((json) => {
+        json['news'].forEach(element => {
+            if(!(element=="Le immagini - Tutti i VIDEO")){
+              document.querySelector('.div2').innerHTML+="<p>"+element+"</p>";
+            }
+            
+        });
+              
     }
+  
+  );
 }
 
-function getPath(){
-    libxml_use_internal_errors(true);
-    $dom = new DomDocument;
-    $xpath = new DomXPath($dom);
-    global $response;
+function getDate(){
+  setInterval(() => {
+    fetch('http://localhost/SCRAPER/scraper.php')
+  .then((response) => response.json())
+  .then((json) => {     
+            document.querySelector('.div').innerHTML="<p>"+"ORA DI AGGIORNAMENTO: "+json.date+"</p>";
+        });
+  }, 1000);
+  
+              
 }
-
-function getData(){
-    global $response;
-    $response['date'] = date("d-m-Y-h:i");
-
-    //echo date("d-m-Y");
-    //echo '<br>';
-    //echo date("h:i:sa");
-}
-function getNews(){
-    global $response;
-    libxml_use_internal_errors(true);
-    $dom = new DomDocument;
-    $dom->loadHTMLFile("https://www.ansa.it/");
-    $xpath = new DomXPath($dom);
-    $news = $xpath->query("//h3[contains(@class, 'news-title area-primopiano')]");
-    foreach ($news as $i => $node) {
-        $response['news'][]=$node->nodeValue;
-    }
-}
-
-?>
