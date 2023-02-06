0d7f09b13c578801e9bf7a5d0b58e3743bf33983
LeonardoMarini <100464046+LeonardoMarini@users.noreply.github.com>
2023-02-06 21:41:12 +0100

    Update javascript.js

--- SCRAPER/javascript.js
+++ SCRAPER/javascript.js
@@ -1,28 +1,69 @@
+<?php
+global $response;
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
 
-function fetchScraper(){
-    fetch('http://localhost/SCRAPER/scraper.php')
-  .then((response) => response.json())
-  .then((json) => {
-        json['news'].forEach(element => {
-            if(!(element=="Le immagini - Tutti i VIDEO")){
-              document.querySelector('.div2').innerHTML+="<p>"+element+"</p>";
-            }
-            
-        });
-              
+
+getPath();
+getData();
+getNews();
+getPhoto();
+echo json_encode($response);
+function crawl_page($icone)
+{
+    $breaker = false;
+    foreach ($icone as $element) {
+        if ($breaker) {
+            break;
+        }
+        // Extracting value of src attribute of
+        // the current image object
+        $src = $element->getAttribute('src');
+        // Given Output as image with extracted attribute,
+        // you can print value of those attributes also
+        //echo $src;
+        return $src;
+        //$breaker = true;
+
     }
-  
-  );
 }
 
-function getDate(){
-  setInterval(() => {
-    fetch('http://localhost/SCRAPER/scraper.php')
-  .then((response) => response.json())
-  .then((json) => {     
-            document.querySelector('.div').innerHTML="<p>"+"ORA DI AGGIORNAMENTO: "+json.date+"</p>";
-        });
-  }, 1000);
-  
-              
+function getPath(){
+    libxml_use_internal_errors(true);
+    $dom = new DomDocument;
+    $xpath = new DomXPath($dom);
+    global $response;
 }
+
+function getData(){
+    global $response;
+    $response['date'] = date("d-m-Y-h:i");
+
+    //echo date("d-m-Y");
+    //echo '<br>';
+    //echo date("h:i:sa");
+}
+function getNews(){
+    global $response;
+    libxml_use_internal_errors(true);
+    $dom = new DomDocument;
+    $dom->loadHTMLFile("https://www.ansa.it/");
+    $xpath = new DomXPath($dom);
+    $news = $xpath->query("//h3[contains(@class, 'news-title area-primopiano')]");
+    foreach ($news as $i => $node) {
+        $response['news'][]=$node->nodeValue;
+    }
+}
+
+function getPhoto(){
+    global $response;
+    libxml_use_internal_errors(true);
+    $dom = new DomDocument;
+    $dom->loadHTMLFile("https://www.ansa.it/");
+    $xpath = new DomXPath($dom);
+    $news = $xpath->query("//img");
+    foreach ($news as $i => $node) {
+        $response['img'][]=$node->nodeValue;
+    }
+}
+?>
