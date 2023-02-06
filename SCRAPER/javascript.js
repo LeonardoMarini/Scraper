c70fbde8b0691e2c3e4beb761ea3b2d4479ffe12
LeonardoMarini <100464046+LeonardoMarini@users.noreply.github.com>
2023-02-06 21:39:45 +0100

    Update javascript.js

--- SCRAPER/javascript.js
+++ SCRAPER/javascript.js
@@ -4,11 +4,25 @@ function fetchScraper(){
   .then((response) => response.json())
   .then((json) => {
         json['news'].forEach(element => {
-            document.querySelector('.div').innerHTML+="<p>"+element+"</p>";
-        });
+            if(!(element=="Le immagini - Tutti i VIDEO")){
+              document.querySelector('.div2').innerHTML+="<p>"+element+"</p>";
+            }
             
-        
+        });
+              
     }
   
   );
+}
+
+function getDate(){
+  setInterval(() => {
+    fetch('http://localhost/SCRAPER/scraper.php')
+  .then((response) => response.json())
+  .then((json) => {     
+            document.querySelector('.div').innerHTML="<p>"+"ORA DI AGGIORNAMENTO: "+json.date+"</p>";
+        });
+  }, 1000);
+  
+              
 }
